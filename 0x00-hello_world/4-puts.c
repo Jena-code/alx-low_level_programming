829c9a1012f744de57d0f66d02f0ad34b32e96db
Jenasom Jerry <jenasom494@gmail.com>
2022-09-08 10:11:13 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - print the string in the put function
+*
+*Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+8 Return: 0
+*/
+
+int main(void)
+{
+
+     puts("\"Programming islike a building a multilingual puzzle");
+     return (0);
+}
