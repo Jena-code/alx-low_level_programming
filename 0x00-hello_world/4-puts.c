5cf4b4f7181d923481a6c881381c826434794e8a
Jenasom Jerry <jenasom494@gmail.com>
2022-09-08 11:24:45 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 /**
-*main - print the string in the put function
+* main - print the string in the put function
 *
-*Description: using the main function
-* this program prints "programming is like building a multilingual puzzle
-8 Return: 0
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
-
 int main(void)
 {
-
-     puts("\"Programming islike a building a multilingual puzzle");
-     return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
